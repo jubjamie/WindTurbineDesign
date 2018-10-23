596622e2f952d009c2d67b10c7d20d61e6385d70
Jamie Cooke <jamie@iamjamiecooke.com>
2018-10-23 17:26:29 +0100

    Revert "Add addpathtest comments"
    This reverts commit 5ecae1361105e5f7072519b0205360dc0600eafe.

--- addpathtest.m
+++ addpathtest.m
@@ -1,6 +1,4 @@
 %%Tests the addpath functionality of MATLAB
 addpath('lib');
-%Use example function provided in lib folder.
 [Cl,Cd] = ForceCoefficient(0.2,12000);
-%Print out.
 disp([num2str(Cl) '-' num2str(Cd)]);
