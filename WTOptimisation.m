5134456989fa4609130e40317d07f31acfc992e8
Jamie Cooke <jamie@iamjamiecooke.com>
2018-10-23 16:47:15 +0100

    Move moodle files to lib

--- WTOptimisation.m
+++
@@ -1,6 +0,0 @@
-function [] = WTOptimisation()
-%4: OPTIMISATION - use fminsearchbnd to optimise ?0, ?tw, and cgrad for
-%maximum AEP.
-
-
-end
---
+++ lib/WTInducedCalcs.m
@@ -0,0 +1,9 @@
+function [a, adash, phi, Cn, Ct] = WTInducedCalcs(a, adash, V0, omega, y, theta, Chord, B)
+%1: SINGLE ELEMENT: use an iterative solution to find the values of a,
+%adash, phi, Cn and Ct at a particular radius.
+
+
+
+
+end
+
