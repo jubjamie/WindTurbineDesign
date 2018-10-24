3de70f13a63592c14c2253a788b5d3539741a50f
Jamie Cooke <jamie@iamjamiecooke.com>
2018-10-24 17:20:14 +0100

    Implement Moment Calculations

--- WTSingleVelocity.m
+++ WTSingleVelocity.m
@@ -1,4 +1,4 @@
-function [Mt, Mn,S2] = WTSingleVelocity(V0, Theta0, ThetaTwist, MeanChord, c_grad, TipRadius,RootRadius, omega, B)
+function [Mt, Mn,S2,Power] = WTSingleVelocity(V0, Theta0, ThetaTwist, MeanChord, c_grad, TipRadius,RootRadius, omega, B)
 %2: WHOLE ROTOR - loop WTInducedCalcs to find the values for all radii,
 %then integrate these to get the normal and tangential moment at the blade
 %root.
@@ -6,7 +6,7 @@ global Hhub dhub Rmin Rmax c_mean M_rootmax F_Ymax rho_blade EI_blade Vmin Vmax 
 
 N=19 %set radius node count
 radius_delta=(TipRadius-RootRadius)/(N); %Increment in radius
-S2=zeros(N,8); %Creat empty matrix to hold values
+S2=zeros(N,11); %Creat empty matrix to hold values
 
 for j=1:N
     local_radius=RootRadius+((j-1)*radius_delta)+(radius_delta/2); % Calculate local radius from centre increment
@@ -17,11 +17,15 @@ for j=1:N
     S2(j,1:8)=[local_radius, a_s1, adash_s1, phi_s1, Cn_s1, Ct_s1, tol_s1, i_s1];
     
     %Calculate each moment
-    
+    Vrel=((V0*(1-a_s1))^2 + (omega*local_radius*(1+adash_s1))^2)^0.5;
+    Mt=(0.5*1.225*local_chord*Ct_s1*Vrel^2)*radius_delta*local_radius;
+    Mn=(0.5*1.225*local_chord*Cn_s1*Vrel^2)*radius_delta*local_radius;
+    S2(j,9:11)=[Vrel,Mt,Mn];
 
 end
 
-Mt=0;
-Mn=0;
+Mtot_t=sum(S2(:,10));
+Mtot_n=sum(S2(:,11));
+Power=Mtot_t*B*w
 
 end
