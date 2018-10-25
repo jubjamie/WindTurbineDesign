ac6acdddead42fe3846b5bcda1c4d27cffc911e7
Jamie Cooke <jamie@iamjamiecooke.com>
2018-10-25 22:28:01 +0100

    Add createlog function

---
+++ createlog.m
@@ -0,0 +1,16 @@
+function [logid] = createlog()
+%CREATELOG Summary of this function goes here
+%   Detailed explanation goes here
+mydir  = pwd;
+idcs   = strfind(mydir,filesep);
+newdir = mydir(1:idcs(end)-1);
+logstamp=datestr(now,'HH-MM-SS--yyyy-mm-dd');
+logid=fopen(strcat(newdir,'/logs/',logstamp ,'.log'), 'a');
+fprintf(logid,'Log created at: %s\r\n',logstamp);
+fprintf(logid,'Created by %s on %s\r\n',getenv('username'),getenv('computername'));
+fprintf(logid,'Computer: %s\r\n',getenv('computername'));
+fprintf(logid,'OS: %s\r\n',getenv('os'));
+fprintf(logid,'Processors: %s\r\n',getenv('number_of_processors'));
+fprintf(logid,'Threads: %s\r\n',getenv('omp_num_threads'));
+end
+
