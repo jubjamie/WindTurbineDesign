a1311739a2e01c03539ecc9a746f5783b454b34a
Jamie Cooke <jamie@iamjamiecooke.com>
2018-10-25 21:57:14 +0100

    Extend full solution status and update statuses

--- lastSolution.m
+++ lastSolution.m
@@ -4,4 +4,4 @@ progressbar('Calculating Power', 'Solving Rotor', 'Finding Local Induced Flow', 
         [diff, AEP, S3] = WTVelocityRange(x, A, k, w, c_mean, Rmax, Rmin, 3, Vmin, Vmax);
         statustablematrix(S3,{'V0', 'Power', 'Probability', 'AEP', 'Ideal_AEP', 'Difference', 'Efficiency'},false,'Section 3 AEP Nodal Solution','figure',1.3);
         IAEP=sum(S3(:,5));
-        statustablematrix([xdeg(1), xdeg(2), xdeg(3), AEP,IAEP,(IAEP-AEP),(AEP/IAEP)],{'Theta','Theta_Twist','c_grad','AEP', 'Ideal_AEP', 'Diff', 'Fraction'},'status/optSol.png','Section 3 AEP Total Solution','figure',1.2);
+        statustablematrix([xdeg(1), xdeg(2), xdeg(3), AEP,IAEP,(IAEP-AEP),(AEP/IAEP)],{'Theta','Theta_Twist','c_grad','AEP', 'Ideal_AEP', 'Diff', 'Fraction'},'status/optSol.png','Section 3 AEP Total Solution','figure',1.3);
