67fd93cb6ee15b2b5cb9daf9624065780e9f5ff5
Jamie Cooke <jamie@iamjamiecooke.com>
2018-10-26 00:40:26 +0100

    Reduce Iteration count for testing and change lower theta boundary.

--- main.m
+++ main.m
@@ -68,7 +68,7 @@ statustablematrix(S3,{'V0', 'Power', 'Probability', 'AEP', 'Ideal_AEP', 'Differe
 % Create Log File
 logid=createlog('Part B Optimiser');
 
-maxiters=5;
+maxiters=2;
 progressbar('Calculating Power', 'Solving Rotor', 'Finding Local Induced Flow', 'Optimisation');
 %progressbar([],[],[], (1/maxiters));
 opts = optimset('fminsearch');
@@ -77,7 +77,7 @@ opts.TolX = 0.0001; %Tolerance on the variation in the parameters
 opts.TolFun = 0.001; %Tolerance on the error
 opts.OutputFcn = @optMonitor; %Tolerance on the error
 opts.MaxIter = maxiters-1; %Max number of iterations
-[x, diff, exitflag] = fminsearchbnd(@aepCost, [deg2rad(12) deg2rad(-0.4) 0], [deg2rad(0) deg2rad(-2) 0], [deg2rad(20) deg2rad(2) 0.9], opts);
+[x, diff, exitflag] = fminsearchbnd(@aepCost, [deg2rad(12) deg2rad(-0.4) 0], [deg2rad(4) deg2rad(-2) 0], [deg2rad(20) deg2rad(2) 0.9], opts);
 xdeg=[rad2deg(x(1)),rad2deg(x(2)),x(3)];
 if exitflag==1
     disp('Optimiser SOLVED.');
