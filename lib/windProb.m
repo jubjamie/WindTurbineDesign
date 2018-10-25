931097273567dcfd99382c8e98be739c813000f1
Jamie Cooke <jamie@iamjamiecooke.com>
2018-10-25 17:50:54 +0100

    Add proability code to windProb

--- lib/windProb.m
+++ lib/windProb.m
@@ -1,7 +1,14 @@
-function [outputArg1,outputArg2] = windProb(inputArg1,inputArg2)
+function [prob] = windProb(A, k, Vi1, Vi2)
 %WINDPROB Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+
+%Vi_1 prob
+vi1prob=exp(-(Vi1/A)^k);
+%Vi_2 prob
+vi2prob=exp(-(Vi2/A)^k);
+
+%Difference is probability for band.
+
+prob=vi1prob-vi2prob;
 end
 
