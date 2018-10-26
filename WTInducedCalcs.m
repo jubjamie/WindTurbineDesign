fc1ca4eab8d5bde52ac11552f77595cc60abe68e
Jamie Cooke <jamie@iamjamiecooke.com>
2018-10-26 17:39:55 +0100

    Major S1 Convergence fixes
    -Fix error calculation when adash being held to zero.
    -Add comments in to some calculations
    -Reduce adash loop limit.
    -Tighten ambiguous bracketing
    -Stop bounding convergence

--- WTInducedCalcs.m
+++ WTInducedCalcs.m
@@ -8,8 +8,7 @@ etol=0.0001;
 curr_tol=1;
 solfnd=false;
 looplimit=500;
-adash_looplimit=200;
-negCtFnd=false;
+adash_looplimit=150;
 
 solidity=(B*Chord)/(2*pi*y);
  
@@ -18,38 +17,32 @@ a_in=a;
 adash_in=adash;
 
     for i=1:looplimit
-        progressbar([],[],i/(looplimit+1), []);
-        phi_flow=atan(((1-a_in)*V0)/((1+adash_in)*omega*y));
-        alpha=phi_flow-theta;
-        Vrel=((V0*(1-a_in))^2 + (omega*y*(1+adash_in))^2)^0.5;
-        re=RE(1.225,Vrel,Chord,1.81e-5);
-        [Cl, Cd]=ForceCoefficient(alpha,re);
-        Cn=(Cl*cos(phi_flow))+(Cd*sin(phi_flow));
-        Ct=(Cl*sin(phi_flow))-(Cd*cos(phi_flow));
-        a_out=1/(((4*(sin(phi_flow)^2))/(solidity*Cn))+1);
-        adash_out=1/(((4*(sin(phi_flow)*cos(phi_flow))/(solidity*Ct))-1));
+        progressbar([],[],i/(looplimit+1), []); %Update progress bar for loop
         
-        if Ct<0 && negCtFnd==false 
-            fprintf(logid,'Negative Ct found: Ct=%f, a=%f, adash=%f Calling function: WTInducedCalcs(%f, %f, %f, %f, %f, %f, %f, %f)\r\n',Ct,a_out,adash_out,a, adash, V0, omega, y, theta, Chord, B);
-            negCtFnd=true;
-        end
+        phi_flow=atan(((1-a_in)*V0)/((1+adash_in)*omega*y)); %Calculate phi flow angle
+        alpha=phi_flow-theta; %Calculate alpha
+        
+        Vrel=((V0*(1-a_in))^2 + (omega*y*(1+adash_in))^2)^0.5; %Find relative velocity  the airfoil sees
+        re=RE(1.225,Vrel,Chord,1.81e-5); %Calculate Reynolds Number
+        [Cl, Cd]=ForceCoefficient(alpha,re); %Calculate/Look Up Cl,Cd
+        
+        %Convert to normal and tangential forces.
+        Cn=(Cl*cos(phi_flow))+(Cd*sin(phi_flow)); 
+        Ct=(Cl*sin(phi_flow))-(Cd*cos(phi_flow));
+        
+        a_out=1/(((4*((sin(phi_flow))^2))/(solidity*Cn))+1);
+        adash_out=1/(((4*(sin(phi_flow)*cos(phi_flow)))/(solidity*Ct))-1);
+        
         
         %Test for convergance
-        curr_tol=abs(a_out-a_in)+abs(adash_out-adash_in);
+        if i<adash_looplimit
+            curr_tol=abs(a_out-a_in)+abs(adash_out-adash_in);
+        else
+            curr_tol=abs(a_out-a_in);
+        end
         if curr_tol>etol
             %See if a near boundaries
-            a_in_relax=(0.05*(a_out-a_in))+a_in;
-            if a_in_relax>0.95 || a_in_relax<0
-                if a_out<0
-                    a_in=0;
-                elseif a_out>1
-                    a_in=1;
-                else
-                a_in=a_out;
-                end
-            else
-                a_in=a_in_relax;
-            end
+            a_in=(0.1*(a_out-a_in))+a_in;
             
         %Log adash loop limit
         if i==adash_looplimit
@@ -58,20 +51,8 @@ adash_in=adash;
             
             if i<adash_looplimit
                 %See if adash near boundaries
-                %adash_in_relax=(0.05*(adash_out-adash_in))+adash_in;
-                adash_in_relax=adash_out;
-            if adash_in_relax>0.95 || adash_in_relax<0
-                if adash_out<0
-                    adash_in=0;
-                elseif adash_out>1
-                    adash_in=1;
-                else
-                adash_in=adash_out;
-                end
-            else
-                adash_in=adash_in_relax;
-            end
-                
+                adash_in=(0.05*(adash_out-adash_in))+adash_in;
+               
             else
                 adash_in=0;
                 adash_out=0;
