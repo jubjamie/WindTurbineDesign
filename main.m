3cb0ea1c261d57227e195178aa1904aa34e0f00a
Jamie Cooke <jamie@iamjamiecooke.com>
2018-10-26 23:00:09 +0100

    Experiment with starting point.

--- main.m
+++ main.m
@@ -36,7 +36,7 @@ etol=0.01;
 [logid, logpath]=createlog('Part B Optimiser');
 tic;
 
-maxiters=40;
+maxiters=15;
 progressbar('Calculating Power', 'Solving Rotor', 'Finding Local Induced Flow', 'Optimisation');
 %progressbar([],[],[], (1/maxiters));
 opts = optimset('fminsearch');
@@ -45,7 +45,7 @@ opts.TolX = 0.0001; %Tolerance on the variation in the parameters
 opts.TolFun = 0.001; %Tolerance on the error
 opts.OutputFcn = @optMonitor; %Tolerance on the error
 opts.MaxIter = maxiters-1; %Max number of iterations
-[x, diff, exitflag] = fminsearchbnd(@aepCost, [deg2rad(12) deg2rad(-0.4) 0], [deg2rad(4) deg2rad(-2) 0], [deg2rad(20) deg2rad(-0.1) 0.9], opts);
+[x, diff, exitflag] = fminsearchbnd(@aepCost, [deg2rad(5) deg2rad(-0.8) 0.2], [deg2rad(2) deg2rad(-2) 0], [deg2rad(20) deg2rad(-0.1) 0.9], opts);
 xdeg=[rad2deg(x(1)),rad2deg(x(2)),x(3)];
 if exitflag==1
     disp('Optimiser SOLVED.');
