dd9d03fad9888150af3f418f7103d3c0dfa60062
Jamie Cooke <jamie@iamjamiecooke.com>
2018-11-11 16:31:05 +0000

    Colour and LineStyle lastSolution Power Plot

--- lastSolution.m
+++ lastSolution.m
@@ -7,9 +7,9 @@ statustablematrix([xdeg(1), xdeg(2), xdeg(3), AEP,IAEP,(IAEP-AEP),(AEP/IAEP)],{'
 % Plot graph of powers.
 %S3-1 vs S3-2
 f6=figure(6);
-plot(S3(:,1),(S3(:,4)./1e6));
+plot(S3(:,1),(S3(:,4)./1e6),'-b');
 hold on;
-plot(S3(:,1),(S3(:,5)./1e6));
+plot(S3(:,1),(S3(:,5)./1e6),'-r');
 grid
 xlabel('Wind Speed (m/s)');
 ylabel('Power x Probability (MW)');
@@ -30,7 +30,7 @@ globaldata.flags.overrideLimits=true;
 globaldata.flags.tiploss=true;
 globaldata.flags.overrideLimits=false;
 
-plot(S3_ntl(:,1),(S3_ntl(:,4)./1e6));
+plot(S3_ntl(:,1),(S3_ntl(:,4)./1e6),'--c');
 legend({['Found Solution' newline '$\theta$: ' num2str(round(xdeg(1),2)) '$^\circ$'  ...
     newline '$\theta_{tw}$: ' num2str(round(xdeg(2),2)) '$^\circ$/m' newline...
     '$c_{grad}$: ' num2str(round(xdeg(3),4))], 'Ideal AEP Solution','Tip Losses Neglected'},...
