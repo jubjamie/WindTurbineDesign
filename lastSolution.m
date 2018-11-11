c97140f17540bc1c94b4cae368315dca7690c3d0
Jamie Cooke <jamie@iamjamiecooke.com>
2018-11-11 15:43:48 +0000

    Plot AEP vs Ideal AEP and save to status.

--- lastSolution.m
+++ lastSolution.m
@@ -1,4 +1,21 @@
+close all
 [diff, AEP, S3] = WTVelocityRange(x, globaldata.A, globaldata.k, globaldata.w, globaldata.c_mean, globaldata.Rmax, globaldata.Rmin, 3, globaldata.Vmin, globaldata.Vmax,globaldata);
 statustablematrix(S3,{'V0', 'Power', 'Probability', 'AEP', 'Ideal_AEP', 'Difference', 'Efficiency', 'Tip_Deflection'},false,'Section 3 AEP Nodal Solution','figure',1.3);
 IAEP=sum(S3(:,5));
 statustablematrix([xdeg(1), xdeg(2), xdeg(3), AEP,IAEP,(IAEP-AEP),(AEP/IAEP)],{'Theta','Theta_Twist','c_grad','AEP', 'Ideal_AEP', 'Diff', 'Fraction'},'status/optSol.png','Section 3 AEP Total Solution','figure',1.3);
+
+% Plot graph of powers.
+%S3-1 vs S3-2
+f6=figure(6);
+plot(S3(:,1),(S3(:,4)./1e6));
+hold on;
+plot(S3(:,1),(S3(:,5)./1e6));
+grid
+xlabel('Wind Speed (m/s)');
+ylabel('Power x Probability (MW)');
+legend({['Found Solution' newline '$\theta$: ' num2str(round(xdeg(1),2)) '$^\circ$'  ...
+    newline '$\theta_{tw}$: ' num2str(round(xdeg(2),2)) '$^\circ$/m' newline...
+    '$c_{grad}$: ' num2str(round(xdeg(3),4))], 'Ideal AEP Solution'},...
+    'Location','Northeast','Interpreter','latex','FontSize',12);
+set(gcf,'position',[200,200,650,450])
+saveas(f6,'status/powerLastSolution.png');
